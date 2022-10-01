142685744334c48561b70dff9f569f1badcb18ed
ZxBing0066 <ZxBing0066@gmail.com>
2022-10-01 20:39:23 +0800

    fix version

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,206 @@
+import copy from 'copy-to-clipboard';
+
 import './style.css';
 
+const imageDOM = document.getElementById('image') as HTMLImageElement;
+const mosaicImageDOM = document.getElementById('mosaic-image') as HTMLImageElement;
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const offscreenCanvas = document.getElementById('offscreen') as HTMLCanvasElement;
 const fileDOM = document.getElementById('file') as HTMLInputElement;
+const shadowImageDOM = document.getElementById('shadow-image')!;
+
+const precisionDOM = document.getElementById('precision') as HTMLInputElement;
+const shadowGapDOM = document.getElementById('shadow-gap') as HTMLInputElement;
+const shadowRadiusDOM = document.getElementById('shadow-radius') as HTMLInputElement;
+const dropTransparentDOM = document.getElementById('drop-transparent') as HTMLInputElement;
+const dropWhiteDOM = document.getElementById('drop-white') as HTMLInputElement;
+const dropAlphaDOM = document.getElementById('drop-alpha') as HTMLInputElement;
+const exportButtonDOM = document.getElementById('export') as HTMLButtonElement;
 
 const ctx = canvas.getContext('2d')!;
 const offscreenCtx = offscreenCanvas.getContext('2d')!;
-offscreenCtx.imageSmoothingEnabled = false;
-(offscreenCtx as any).mozImageSmoothingEnabled = false;
-(offscreenCtx as any).webkitImageSmoothingEnabled = false;
-(offscreenCtx as any).msImageSmoothingEnabled = false;
-ctx.imageSmoothingEnabled = false;
-(ctx as any).mozImageSmoothingEnabled = false;
-(ctx as any).webkitImageSmoothingEnabled = false;
-(ctx as any).msImageSmoothingEnabled = false;
+
+let file: File;
+const width = 640;
+const canvasWidth = 640;
+// 精度
+let precision: number = 50;
+let shadowGap: number = 0;
+let shadowRadius: number = 0;
+let dropTransparent: boolean = true;
+let dropAlpha: boolean = false;
+let dropWhite: boolean = false;
+precisionDOM.value = precision + '';
+shadowGapDOM.value = shadowGap + '';
+shadowRadiusDOM.value = shadowRadius + '';
+dropTransparentDOM.checked = dropTransparent;
+dropWhiteDOM.checked = dropWhite;
+dropAlphaDOM.checked = dropAlpha;
+
+document.getElementById('initial')?.addEventListener('click', () => {
+    fileDOM.click();
+});
+document.querySelector('.card-image')?.addEventListener('click', () => {
+    fileDOM.click();
+});
 
 fileDOM.addEventListener('change', e => {
     const files = (e.target as HTMLInputElement).files;
     if (!files?.length) return;
-    const file = files[0];
+    file = files[0];
+    document.getElementById('initial')!.style.display = 'none';
+    document.getElementById('app')!.style.display = 'flex';
+    doMosaic();
+});
+
+precisionDOM.addEventListener('change', e => {
+    const target = e.target as HTMLInputElement;
+    precision = +target.value;
+    doMosaic();
+});
+shadowGapDOM.addEventListener('change', e => {
+    const target = e.target as HTMLInputElement;
+    shadowGap = +target.value;
+    updateShadowImage();
+});
+shadowRadiusDOM.addEventListener('change', e => {
+    const target = e.target as HTMLInputElement;
+    shadowRadius = +target.value;
+    updateShadowImage();
+});
+dropTransparentDOM.addEventListener('change', e => {
+    const target = e.target as HTMLInputElement;
+    dropTransparent = target.checked;
+    updateShadowImage();
+});
+dropWhiteDOM.addEventListener('change', e => {
+    const target = e.target as HTMLInputElement;
+    dropWhite = target.checked;
+    updateShadowImage();
+});
+dropAlphaDOM.addEventListener('change', e => {
+    const target = e.target as HTMLInputElement;
+    dropAlpha = target.checked;
+    updateShadowImage();
+});
+
+const controllerFactory = <T = any>(): [Promise<T>, (result: T) => void, (error: any) => void] => {
+    let success: (result: T) => void, error: (error: any) => void;
+    const controller = new Promise<T>((resolve, reject) => {
+        success = resolve;
+        error = reject;
+    });
+    return [controller, success!, error!];
+};
+
+const readFile = (file: File): Promise<string> => {
+    const [controller, success, error] = controllerFactory();
     const reader = new FileReader();
+
     reader.addEventListener('loadend', e => {
         if (e.target?.result) {
-            const img = document.createElement('img');
-            img.classList.add('obj');
-            img.src = e.target?.result as string;
-            img.onload = () => {
-                offscreenCtx.drawImage(img, 0, 0, 20, 20);
-                const smallImg = offscreenCanvas.toDataURL();
-                const nimg = new Image();
-                nimg.src = smallImg;
-                nimg.onload = () => {
-                    ctx.drawImage(nimg, 0, 0, 200, 200);
-                };
-            };
+            success(e.target.result);
+        } else {
+            error(new Error('Read file fail'));
         }
     });
+    reader.addEventListener('error', e => {
+        error(e);
+    });
     reader.readAsDataURL(file);
+    return controller;
+};
+
+const loadImage = (url: string, imageDOM?: HTMLImageElement) => {
+    const [controller, success, error] = controllerFactory<HTMLImageElement>();
+    const img = imageDOM ?? new Image();
+    img.src = url;
+    img.onload = () => {
+        success(img);
+    };
+    img.onerror = e => {
+        error(e);
+    };
+    return controller;
+};
+
+const doMosaic = async () => {
+    const imgUrl = await readFile(file);
+    await loadImage(imgUrl, imageDOM);
+    const ratio = imageDOM.naturalHeight / imageDOM.naturalWidth;
+    offscreenCanvas.width = precision;
+    offscreenCanvas.height = precision * ratio;
+    canvas.width = canvasWidth;
+    canvas.height = canvasWidth * ratio;
+    // console.log(img.width, img.height, img.clientWidth, img.offsetWidth, img.naturalWidth);
+    // offscreenCtx.clearRect();
+    offscreenCtx.imageSmoothingEnabled =
+        (offscreenCtx as any).mozImageSmoothingEnabled =
+        (offscreenCtx as any).webkitImageSmoothingEnabled =
+        (offscreenCtx as any).msImageSmoothingEnabled =
+            false;
+    offscreenCtx.drawImage(imageDOM, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
+    const smallImgUrl = offscreenCanvas.toDataURL();
+    const smallImg = await loadImage(smallImgUrl, mosaicImageDOM);
+
+    ctx.imageSmoothingEnabled =
+        (ctx as any).mozImageSmoothingEnabled =
+        (ctx as any).webkitImageSmoothingEnabled =
+        (ctx as any).msImageSmoothingEnabled =
+            false;
+    ctx.drawImage(smallImg, 0, 0, canvasWidth, canvasWidth * ratio);
+    updateShadowImage();
+};
+
+const updateShadowImage = () => {
+    const ratio = imageDOM.naturalHeight / imageDOM.naturalWidth;
+    const size = (width / precision) | 0;
+    shadowImageDOM.style.height = shadowImageDOM.style.width = Math.max(size - shadowGap, 1) + 'px';
+    const boxShadow = outputBoxShadow(size);
+    shadowImageDOM.style.boxShadow = boxShadow || 'none';
+    shadowImageDOM.parentElement!.style.height = width * ratio + 'px';
+    shadowImageDOM.style.borderRadius = shadowRadius + '%';
+};
+
+function rgbToHex(r: number, g: number, b: number) {
+    if (r > 255 || g > 255 || b > 255) throw 'Invalid color component';
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+const outputBoxShadow = (size: number) => {
+    const shadowArr = [];
+    const ratio = imageDOM.naturalHeight / imageDOM.naturalWidth;
+    const unit = canvasWidth / precision;
+    for (let y = 0; y < precision * ratio; y++) {
+        for (let x = 0; x < precision; x++) {
+            const p = ctx.getImageData(x * unit, y * unit, 1, 1).data;
+            if (dropTransparent && p[3] === 0) {
+                continue;
+            }
+            if (dropWhite && p[3] !== 0 && p[0] === 255 && p[1] === 255 && p[2] === 255) {
+                continue;
+            }
+            const colorInfo = [...p];
+            colorInfo.length = 4;
+            const color = dropAlpha
+                ? '#' + ('000000' + rgbToHex(p[0], p[1], p[2])).slice(-6)
+                : `rgba(${colorInfo.map((v, i) => (i === 3 ? (v / 255).toFixed(3) : v)).join(',')})`;
+            shadowArr.push(`${color} ${x * size}px ${y * size}px` + (y === 0 && x === 0 ? ` 0 ${size}px inset` : ''));
+        }
+    }
+    return shadowArr.join(',');
+};
+
+exportButtonDOM.addEventListener('click', () => {
+    const size = (width / precision) | 0;
+    const shadow = outputBoxShadow(size);
+    copy(`
+.mosaic {
+    box-shadow: ${shadow};
+    width: ${size}px;
+    height: ${size}px;
+    border-radius: ${shadowRadius}%;
+}
+`);
+    alert('已复制到剪切板');
 });
